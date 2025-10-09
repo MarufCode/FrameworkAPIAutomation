dc88c6b4bb437791140aa0099a4a3908154083d0
Maruf <marufsherikar32@gmail.com>
2025-10-09 13:03:14 +0530

    Folder Structure is committed

---
+++ src/test/java/org/example/tests/crud/TCCreateBooking.java
@@ -0,0 +1,13 @@
+package org.example.tests.crud;
+
+import org.testng.annotations.Test;
+
+public class TCCreateBooking {
+
+
+
+    @Test
+    public void testPositivePostReq(){
+
+    }
+}
---
+++ src/test/java/org/example/tests/e2e/TC1.java
@@ -0,0 +1,7 @@
+package org.example.tests.e2e;
+
+public class TC1 {
+
+
+    // Create Booking -> Update Booking -> Delete Booking
+}
